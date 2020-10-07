877aff3561f81136d946c076a5f74581eb9c904d
sambarton10 <69605114+sambarton10@users.noreply.github.com>
2020-10-06 21:47:15 -0500

    Added a python file for final answer, I will work in jupyter notebook but save my final copy in a python file.

---
+++ PandasChallenge.py
@@ -0,0 +1,17 @@
+# Dependencies and Setup
+import pandas as pd
+
+# File to Load (Remember to Change These)
+school_data_to_load = "/Users/sambarton/pandas-challenge/Resources/schools_complete.csv"
+student_data_to_load = "/Users/sambarton/pandas-challenge/Resources/students_complete.csv"
+
+# Read School and Student Data File and store into Pandas DataFrames
+school_data = pd.read_csv(school_data_to_load)
+student_data = pd.read_csv(student_data_to_load)
+
+# Combine the data into a single dataset.  
+school_data_complete = pd.merge(student_data, school_data, how="left", on=["school_name", "school_name"])
+
+
+df = pd.DataFrame(school_data_complete, columns = ["Total Schools"])
+
